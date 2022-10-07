b617ede13d7815273bf39552405b4aa99dc4957a
dloyolar <dieegolr@gmail.com>
2022-10-07 10:56:50 -0300

    ✨ feat(counterSlice): add reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { useDispatch, useSelector } from 'react-redux';
+
 import './App.css';
+import { increment, decrement, incrementBy } from './store/slices/counter';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <p>count is {counter}</p>
+        <button onClick={() => dispatch(increment())}>Increment</button>
+        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <button onClick={() => dispatch(incrementBy(2))}>Increment by 2</button>
       </div>
     </div>
   );
