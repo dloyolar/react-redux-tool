9a7babff73178359e2ca2b9ccebe84d0c4f5ea17
dloyolar <dieegolr@gmail.com>
2022-10-08 18:37:00 -0300

    ✨ feat: add landing for RTK Query

---
+++ src/TodoApp.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useGetTodosQuery } from './store/apis/todosApi';
+
+export const TodoApp = () => {
+  const { data: todos = [], isLoading } = useGetTodosQuery();
+
+  return (
+    <>
+      <h1>Todos - RTQ Query</h1>
+      <hr />
+      <h4>isLoading: {isLoading ? 'True' : 'False'}</h4>
+      <pre>...</pre>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <strong>{todo.completed ? 'DONE' : 'Pending'}</strong> {todo.title}
+          </li>
+        ))}
+      </ul>
+      <button>Next Todo</button>
+    </>
+  );
+};
